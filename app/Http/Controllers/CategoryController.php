f0983ac36861f69d55be7697f294421a4c2f9114
Kittipop Duangjai <mrkittipopduangjai@gmail.com>
2020-08-16 12:24:05 +0700

    KhaNhom

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function create(){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function store(){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function edit($id){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function update($id){
+        //ข้างในนี้ไม่มีอะไร
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function create(){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function store(){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function edit($id){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function update($id){
+        //ข้างในนี้ไม่มีอะไร
+    }
+    public function delete($id){
+        //ข้างในนี้ไม่มีอะไร
+    }
+}
